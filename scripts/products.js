03ed6eaed314d6cb15f9acb2ab1a9ab6c79e0f93
unknown <edvan.s.pereira@aluno.senai.br>
2024-05-29 21:47:23 -0300

    tentando colocar imagen

---
+++ scripts/products.js
@@ -0,0 +1,75 @@
+let produtos = [
+    {
+        img: "img/hamb-1.jpg",
+        dsImg: "hamburger",
+        nome: "Burguer 100% Angus",
+        valor: 15.00,
+        fatias8: 45.00,
+        fatias12: 55.00
+    },
+    {
+        img: "img/hamb-2.jpg",
+        dsImg: "pizza de calabresa",
+        nome: "Calabresa",
+        fatias6: 35.00,
+        fatias8: 75.00,
+        fatias12: 55.00
+    },
+    {
+        img: "img/hamb-3.jpg",
+        dsImg: "pizza sabor portuguesa",
+        nome: "Portuguesa",
+        fatias6: 35.00,
+        fatias8: 15.00,
+        fatias12: 55.00
+    },
+    {
+        img: "img/hamb-4.jpg",
+        dsImg: "pizza de strogonoff",
+        nome: "Strogonoff",
+        fatias6: 35.00,
+        fatias8: 45.00,
+        fatias12: 55.00
+    },
+    {
+        img: "img/hamb-5.jpg",
+        dsImg: "pizza de chocolate",
+        nome: "Chocolate",
+        fatias6: 35.00,
+        fatias8: 45.00,
+        fatias12: 55.00
+    },
+    {
+        img: "img/hamb-6.jpg",
+        dsImg: "pizza sabor sensação",
+        nome: "Sensação",
+        fatias6: 35.00,
+        fatias8: 95.00,
+        fatias12: 55.00
+    },
+    {
+        img: "img/hamb-7.jpg",
+        dsImg: "pizza sabor confete",
+        nome: "Confete",
+        fatias6: 35.00,
+        fatias8: 45.00,
+        fatias12: 55.00
+    },
+    {
+        img: "img/hamb1.webp",
+        dsImg: "pizza sabor prestigio",
+        nome: "Prestígio",
+        fatias6: 35.00,
+        fatias8: 45.00,
+        fatias12: 55.00
+    },
+    {
+        img: "img/hamb2.webp",
+        dsImg: "pizza sabor prestigio",
+        nome: "Prestígio",
+        fatias6: 35.00,
+        fatias8: 45.00,
+        fatias12: 55.00
+    }
+
+]
